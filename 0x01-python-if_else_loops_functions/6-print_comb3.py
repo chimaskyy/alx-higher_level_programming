1f439dd163681da7a8209276eb36724947d93be6
chimaskyy <chiamakahj@gmail.com>
2023-09-05 22:29:58 +0100

    print combination of two number

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num in range(10):
+    for num2 in range(10):
+        if num == num2 or num2 < num:
+            continue
+        if num != 8 or num2 != 9:
+            print("{}{}".format(num,num2), end=", ")
+        else:
+            print("{}{}".format(num, num2), end="\n")
