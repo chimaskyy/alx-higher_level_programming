c9cd04099a9094c69c2b24c5618eb9172aae11b5
chimaskyy <chiamakahj@gmail.com>
2023-09-05 20:20:46 +0100

    correct code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 for alpha in range(97, 122):
-    if alpha == 101 or alpha == 113:
-        continue
-    print("{}".format(chr(alpha)), end="")
+    if (chr(alpha) != 'q' and chr(alpha) != 'e'):
+         print("{}".format(chr(alpha)), end="")
