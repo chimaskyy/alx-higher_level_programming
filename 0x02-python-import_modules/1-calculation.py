64e099827a53edc795b47c713cde85851441268a
chimaskyy <chiamakahj@gmail.com>
2023-09-07 20:13:30 +0100

    do some maths

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculator_1 as calc
+    a = 10
+    b = 5
+    result = calc.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    result2 = calc.sub(a, b)
+    print("{} - {} = {}".format(a, b, result2))
+    result3 = calc.mul(a, b)
+    print("{} * {} = {}".format(a, b, result3))
+    result4 = calc.div(a, b)
+    print("{} / {} = {}".format(a, b, result4))
