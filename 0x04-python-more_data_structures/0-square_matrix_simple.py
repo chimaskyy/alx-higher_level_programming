dcdd1fe758315e20be4d33cb5220bf9b8fcf115c
chimaskyy <chiamakahj@gmail.com>
2023-09-13 21:03:28 +0100

    compute square value of integers in matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return matrix
+    new_l = []
+    for i in matrix:
+        new_l.append(list(map(lambda n: n ** 2, i)))
+    return new_l
+
+
