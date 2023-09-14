0d143c3e8428c5257a107a9c02910c44e7f896b0
chimaskyy <chiamakahj@gmail.com>
2023-09-14 14:18:27 +0100

    delete

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    score = sum(map(lambda x: x[0] * x[1], my_list))
-    weight = sum(x[1] for x in my_list)
-    return score / weight
+    scores = sum(map(lambda x: x[0] * x[1], my_list))
+    total_weight = sum(x[1], for x in my_list)
+    return scores / total_weight
