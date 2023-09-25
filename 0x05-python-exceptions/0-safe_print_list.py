d3c742c2e9e765efcdd06b892e53980b66d8b53a
chimaskyy <chiamakahj@gmail.com>
2023-09-25 21:15:01 +0100

    prints an integer with "{:d}"

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for nums in range(x):
             print(my_list[nums], end='')
             count += 1
-        print ()
+        print()
     except IndexError:
         print()
         return count
