975afa482dd8c2ee01158dd8a1f0fb5155eeddaf
chimaskyy <chiamakahj@gmail.com>
2023-09-26 22:35:52 +0100

    Define a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Square with size"""
+
+
+class Square:
+    """Empty Square class"""
+    def __init__(self, size=0):
+        """private size attribute initialized for Square instance"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Find area of current square"""
+        return self.__size ** 2
