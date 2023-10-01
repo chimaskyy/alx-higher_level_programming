450c68e42aa831ac9fa1a08e10d89dbbdecd18da
chimaskyy <chiamakahj@gmail.com>
2023-10-01 16:36:25 +0100

    TDD to the rescue

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is a function that adds two integers
+Returns sum of two integers
+"""
+
+def add_integer(a, b=98):
+    """Takes two parameter
+    Returns the sum of the two parameters
+    """
+    if type(a) is float:
+        a = int(a)
+    elif type(b) is float:
+        b = int(b)
+    elif type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    elif type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,7 @@
+#Python - Test-driven development
+
+## In the realm of software engineering, there exist two distinct types of engineers. The first kind diligently composes code but departs without monitoring its execution, placing faith in its seamless operation. In contrast, the second kind, a more meticulous group, meticulously crafts unit tests to validate their code's functionality
+
+```
+Testing is fun
+```
