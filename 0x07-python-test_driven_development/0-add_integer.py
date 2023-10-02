6a2f9c0e04d5eb7ca41fb89efffc3da175a991b2
chimaskyy <chiamakahj@gmail.com>
2023-10-02 11:29:19 +0100

    not good changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,6 +4,7 @@ This is a function that adds two integers
 Returns sum of two integers
 """
 
+
 def add_integer(a, b=98):
     """Takes two parameter
     Returns the sum of the two parameters
@@ -17,4 +18,3 @@ def add_integer(a, b=98):
     elif type(b) not in (int, float):
         raise TypeError('b must be an integer')
     return int(a) + int(b)
-
