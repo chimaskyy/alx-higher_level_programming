1511bb3203609f44853ac716dd6f3cac353d6bf7
chimaskyy <chiamakahj@gmail.com>
2023-10-02 20:08:04 +0100

    pycodestyle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,17 +8,18 @@ class Rectangle:
     """Defined class rectangle"""
 
     def __init__(self, width=0, height=0):
+        """create instances and initialize two attribute"""
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
-         """Gets width"""
-         return self.__width
+        """Width getter to access width attribute"""
+        return self.__width
 
     @width.setter
     def width(self, width):
-        """width setter"""
+        """width setter, sets private attribute __width to provided value"""
         if type(width) is not int:
             raise TypeError('width must be an integer')
         elif width < 0:
@@ -37,4 +38,3 @@ class Rectangle:
         elif height < 0:
             raise ValueError('height must be >= 0')
         self.__height = height
-
