382e47fad5e5fbe1dc1136e8151a45f6f3ad0319
chimaskyy <chiamakahj2@.com>
2023-10-03 09:47:42 +0100

    more test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 """
-This module prints text
-Prints 2 new line after . , : , and ?
+    This module contains function that handles text indenting
 """
+
 
 def text_indentation(text):
     """
-    Function that prints text
-    Prints 2 new line after chars . , ? and :
-    Arg:
-        text: Text to print
+       function that prints a text with 2 new lines after each
+        of these chars: ., ? and :
     """
-    for chars in text:
-        print(chars, end="")
-        if chars in [".", ":", "?"]:
-            print("\n\n", end="")
     if type(text) is not str:
-        raise TypeError('text must be a string')
+        raise TypeError("text must be a string")
+
+    start = 0
+    indenter = [".", "?", ":"]
+    for chars in text:
+        """check if string is whitespace"""
+        if chars.isspace() and start == 0:
+            continue
+        elif chars in indenter:
+            print("{}\n\n".format(chars), end="")
+            start = 0
+            continue
+        else:
+            print("{}".format(chars), end="")
+            start += 1
