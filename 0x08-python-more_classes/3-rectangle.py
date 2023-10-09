81964c95345c4f6cb9e56944b8f8b957c9d707cb
chimaskyy <chiamakahj2@.com>
2023-10-10 00:05:40 +0100

    find area

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
         if type(width) is not int:
             raise TypeError('width must be an integer')
         elif width < 0:
-            raise valueError('width must be >= 0')
+            raise ValueError('width must be >= 0')
         self.__width = width
 
     @property
