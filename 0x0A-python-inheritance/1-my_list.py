1fca82ff28dcb1b1d478077f698f84c5858823cf
chimaskyy <chiamakahj@gmail.com>
2023-10-09 20:46:04 +0100

    executable

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-'''
+"""
 This  module contains a single function
 Prints a list in sorted form
-'''
+"""
 
 
 class MyList(list):
 
-    '''Defined class'''
+    """Defined class"""
 
     def print_sorted(self):
-        '''Function that print list in ascending order'''
+        """Function that print list in ascending order"""
+
     new_list = sorted(self)
     print(new_list)
