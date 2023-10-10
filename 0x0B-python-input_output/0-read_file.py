2de463356e99e7d272c5b792e0d908ea826681ad
chimaskyy <chiamakahj@gmail.com>
2023-10-10 17:45:25 +0100

    function that reads txt file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+The module contains a single function
+'''
+
+
+def read_file(filename=""):
+    '''
+    Function that reads a text file and
+    prints its output to stdout
+    '''
+    with open(filename,'r', encoding="utf-8") as f:
+        data = f.read()
+        print(data)
