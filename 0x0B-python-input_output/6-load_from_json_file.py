7b9cca9372fca7347e4486eb07c4968c2e75178a
chimaskyy <chiamakahj@gmail.com>
2023-10-10 21:10:46 +0100

    function that creates an Object from a JSON file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,10 +5,10 @@ This module contains a single function
 import json
 
 
-def save_to_json_file(my_obj, filename):
+def load_from_json_file(filename):
     '''
     Function that writes an Object to a text file
     using a JSON representation
     '''
-    with open(filename, 'w') as f:
-        json.dump(my_obj, f)
+    with open(filename, 'r') as f:
+        return json.load(f)
