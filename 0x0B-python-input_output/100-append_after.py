e3cd499e1c0d3d997fb49d81e9f19bb33a9b5930
chimaskyy <chiamakahj@gmail.com>
2023-10-12 02:41:01 +0100

    pycodestyle

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,11 +3,12 @@
 This module contains a single function
 '''
 
+
 def append_after(filename="", search_string="", new_string=""):
     '''
     This function inserts a line of text to a file
     after another line'''
-    
+
     with open(filename, 'r', encoding="utf-8")as f:
         data = f.readlines()
 
@@ -16,5 +17,3 @@ def append_after(filename="", search_string="", new_string=""):
             f.write(line)
             if search_string in line:
                 f.write(new_string + '\n')
-
-
