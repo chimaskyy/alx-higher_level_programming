cebe6b224f9c470e9924aa97d2380a35b42e2ad7
chimaskyy <chiamakahj@gmail.com>
2023-10-13 11:17:55 +0100

    more

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,17 @@
 '''This module contains a class'''
 
 
-class base:
-    '''This is the base class'''
+class Base:
+    '''This is the base class
+
+    '''
+    '''private class attribute'''
     __nb_objects = 0
-    __init__(self, id=None):
+    
+    #class constructor
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            self.id = Base.__nb_objects
+            Base.__nb_objects += 1
