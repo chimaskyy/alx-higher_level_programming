3c2ccd517d6b98431f4028b8a9228b8bd17b36a4
chimaskyy <chiamakahj@gmail.com>
2023-11-16 17:04:14 +0100

    foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,6 +8,7 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    state_id INT FOREIGN KEY(id),
+    state_id INT 
+    FOREIGN KEY (state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
