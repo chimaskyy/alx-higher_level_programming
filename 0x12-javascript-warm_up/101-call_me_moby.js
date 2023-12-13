46444444ff7e61e634e689b6c374a53a824dc367
chimaskyy <chiamakahj@gmail.com>
2023-12-13 12:19:58 +0100

    increment and call a function

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,11 +2,11 @@
 
 //  executes x times a function
 
-function callMeMoby(x, theFunction) {
-	let i;
+function callMeMoby (x, theFunction) {
+  let i;
 
-	for (i = 0; i < x; i++) {
-		theFunction();
-	}
+  for (i = 0; i < x; i++) {
+    theFunction();
+  }
 }
 exports.callMeMoby = callMeMoby;
