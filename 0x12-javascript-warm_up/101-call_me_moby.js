384033ec77c8b4ac577f7980d63cf18b95aad127
chimaskyy <chiamakahj@gmail.com>
2023-12-13 12:08:17 +0100

    export function

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+//  executes x times a function
+
+function callMeMoby(x, theFunction) {
+	let i;
+
+	for (i = 0; i < x; i++) {
+		theFunction();
+	}
+}
+exports.callMeMoby = callMeMoby;
