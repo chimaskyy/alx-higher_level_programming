c1163565501b59cb31e1e47f99e1fe496c0a984c
chimaskyy <chiamakahj@gmail.com>
2023-12-14 21:57:56 +0100

    print square

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -15,7 +15,7 @@ class Rectangle {
     for (i = 0; i < this.height; i++) {
       let row = '';
       for (j = 0; j < this.width; j++) {
-        row += '#';
+        row += 'X';
       }
       console.log(row);
     }
