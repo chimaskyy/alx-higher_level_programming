928e1f28bea878ddc66f8e594b536431ab8d6b98
chimaskyy <chiamakahj@gmail.com>
2023-12-15 11:07:57 +0100

    Print number of call

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,10 +3,10 @@
 // reverse a list
 
 exports.esrever = function (list) {
-	let lenList = list.length;
-	let new_list = []
-	for (let i = 0; i < lenList; i++) {
-		new_list[i] = list[lenList - i -1];
-	}
-	return new_list;
+  const lenList = list.length;
+  const newList = [];
+  for (let i = 0; i < lenList; i++) {
+    newList[i] = list[lenList - i - 1];
+  }
+  return newList;
 };
