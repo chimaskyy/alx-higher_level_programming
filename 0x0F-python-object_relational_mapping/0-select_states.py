63fb414f603b3a17c88686465f0bd485f87ccc3f
chimaskyy <chiamakahj@gmail.com>
2023-12-17 10:02:13 +0100

    wrong indentation corrected

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,18 +6,21 @@ database named hbtn_0e_0_usa
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-
-    db = MySQLdb.connect(port=3306, host="localhost",
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        port=3306,
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
+    states = cur.fetchall()
 
     for state in states:
         print(state)
 
-        cur.close()
-        db.close()
+    cur.close()
+    db.close()
