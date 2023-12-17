d76dce37b7c3a2401c4cd9fadeb970109f9e4a51
chimaskyy <chiamakahj@gmail.com>
2023-12-17 14:17:03 +0100

    pass statneame as arg

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+This module takes in an argument and
+displays all values in the states table
+of hbtn_0e_0_usa where 'name' matches the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        port=3306,
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+    curr = db.cursor()
+
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
+    curr.execute(query.format(sys.argv[4]))
+    states = curr.fetchall()
+
+    for state in states:
+        print(state)
+
+    curr.close()
+    db.close()
