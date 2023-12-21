96321988810235a3b63b027e5bacdc43c9f327e2
chimaskyy <chiamakahj2@gmail.com>
2023-12-21 21:39:18 +0000

    print the first result

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module lists all State
+instance from the database"""
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    import sqlalchemy
+    from sqlalchemy.orm import sessionmaker, Session
+    from model_state import Base, State
+    import sys
+
+    con = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                      sys.argv[2],
+                                                      sys.argv[3])
+    engine = create_engine(con, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).first()
+
+    if states:
+        print("{}: {}".format(states.id, states.name))
+    else:
+        print("Nothing")
+
+    session.close()
