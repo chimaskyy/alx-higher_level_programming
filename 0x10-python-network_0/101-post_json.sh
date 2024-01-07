edc5051cca7c3a96ced8ad9d58c806c99205b508
chimaskyy <chiamakahj2@gmail.com>
2024-01-07 01:48:47 -0800

    fixed

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #send JSON post request, JSON file name as second args
-curl -X POST "$1" -H "Content-type: application/JSON" -d @"$2"
+curl -X POST -H "Content-type: application/JSON" -d @"$2" "$1"
