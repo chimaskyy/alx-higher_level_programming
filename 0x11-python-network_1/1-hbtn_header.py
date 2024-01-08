90c3ad290c47d7f0d52a17724606e4e2e7f96e16
chimaskyy <chiamakahj2@gmail.com>
2024-01-08 10:56:08 -0800

    new file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-'''This module takes in a URL, sends
-a request to the URL and displays the
-value of the X-Request-Id variable found
-in the header of the response.'''
+"""
+Sends a request to the url passed at CLI and displays the value of the
+X-Request-Id variable found in the header
+"""
 
 import sys
 import urllib.request
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1])
 
-with urllib.request.urlopen(req) as response:
-    request_Id = response.headers['X-Request-Id']
-    print(request_Id)
+    with urllib.request.urlopen(req) as response:
+        xRequestID = response.headers.get('X-Request-Id')
+        print(xRequestID)
