f0709851bb1334779efc1ce7d861fdd1e28394f2
chimaskyy <chiamakahj2@gmail.com>
2024-01-07 20:36:06 -0800

    fetch a value in response body

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys
+import urllib.request
+
+req = urllib.request.Request(sys.argv[1])
+
+with urllib.request.urlopen(req) as response:
+    request_Id = response.headers['X-Request-Id']
+    print(request_Id)
