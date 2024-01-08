a71d4d751434732156ebd91783658445ac37316c
chimaskyy <chiamakahj2@gmail.com>
2024-01-08 03:45:14 -0800

    get value

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' This module sends post request to
+url and display its body '''
+
+import sys
+import requests
+
+url = sys.argv[1]
+email = sys.argv[2]
+data = {
+        'email': email
+        }
+
+req = requests.post(url, data=data)
+res = req.text
+print(res)
