c0ccbd61e13e918c69becfe579b2dcfbb19a13be
chimaskyy <chiamakahj2@gmail.com>
2024-01-07 20:58:19 -0800

    getheader

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,5 @@ import urllib.request
 req = urllib.request.Request(sys.argv[1])
 
 with urllib.request.urlopen(req) as response:
-    request_Id = response.headers['X-Request-Id']
+    request_Id = response.getheader('X-Request-Id')
     print(request_Id)
