2726792439a56d08632db428029cae761218ff99
chimaskyy <chiamakahj2@gmail.com>
2024-01-08 11:07:35 -0800

    check check

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,18 +9,17 @@ of the response
 import urllib.request
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-value = {
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {
         'email': email
         }
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
 
-data = urllib.parse.urlencode(value)
-data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
 
-req = urllib.request.Request(url, data)
-
-with urllib.request.urlopen(req) as response:
-    content = response.read().decode('utf-8')
-    print(content)
+    with urllib.request.urlopen(req) as response:
+        content = response.read().decode('utf-8')
+        print(content)
