300e43fa0cf032cfec9eda785f635b5fb173206e
chimaskyy <chiamakahj2@gmail.com>
2024-01-07 20:00:23 -0800

    fetch url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''This module fetches the url
+"https://alx-intranet.hbtn.io/status" '''
+
+import urllib.request
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+
+with urllib.request.urlopen(req) as response:
+    data = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf content: {}".format(data.decode('utf-8')))
