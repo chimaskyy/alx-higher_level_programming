50756839c9c703c89728f77d60951592bc81dea2
chimaskyy <chiamakahj2@gmail.com>
2024-01-08 09:38:17 -0800

    send post request to url wit a parameter passed

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.'''
+
+import sys
+import requests
+
+if len(sys.argv) == 1:
+    q = ""
+else:
+    q = sys.argv[1]
+url = "http://0.0.0.0:5000/search_user"
+
+r = requests.post(url, data={'q': q})
+
+try:
+    json = r.json()
+    if 'id' in json:
+        print(f"{json['id']} {json['name']}")
+    else:
+        print("No result")
+except ValueError as e:
+    print("Not a valid JSON")
