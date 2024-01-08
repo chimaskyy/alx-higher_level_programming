3b55de766c07639d565901d37b793158db48233b
chimaskyy <chiamakahj2@gmail.com>
2024-01-08 03:59:43 -0800

    erroe code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''This module sends a request to the URL
+displays the body of the response and handles http erro'''
+
+import sys
+import requests
+
+url = sys.argv[1]
+
+req = requests.get(url)
+res = req.status_code
+if res >= 400:
+    print("Error code: {}".format(res))
+else:
+    print(req.text)
